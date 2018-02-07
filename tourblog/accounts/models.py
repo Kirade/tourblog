9f431342d89b2ff821c8844221fdaecb6e629dcc
허재영 <jay.jaeyoung@gmail.com>
2018-02-08 04:30:27 +0900

    accounts Profile Model 추가

--- tourblog/accounts/models.py
+++ tourblog/accounts/models.py
@@ -1,3 +1,12 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    address = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=20)
+
+
+    # phone_number에 대한 validator 추가해야함
+
