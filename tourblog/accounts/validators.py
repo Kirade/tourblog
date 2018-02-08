a64e74be6c806681d0f75d759213d43cc22d3149
허재영 <jay.jaeyoung@gmail.com>
2018-02-08 14:32:53 +0900

    Fix phone_number validator

--- tourblog/accounts/validators.py
+++ tourblog/accounts/validators.py
@@ -3,7 +3,6 @@ from django.forms import ValidationError
 
 
 def phone_number_validator(value):
-    print("This is Validator!!!!!!!!!!!!!!!!!!!!!!!")
     p = re.compile('^01\d{8,9}$')
-    if not re.match(value):
+    if not p.match(value):
         raise ValidationError('{} is not proper phone number'.format(value))
