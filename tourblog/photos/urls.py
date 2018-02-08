5a62f6d6a5e30a367e598da482d2c10cedb8799e
허재영 <jay.jaeyoung@gmail.com>
2018-02-08 20:32:10 +0900

    Photo detail view

--- tourblog/photos/urls.py
+++ tourblog/photos/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'photos'
 urlpatterns = [
     url(r'^list/$', views.photo_list, name='photo_list'),   
+    url(r'^(?P<pk>\d+)/$', views.photo_detail, name='photo_detail'),   
 ]
--- tourblog/photos/views.py
+++ tourblog/photos/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Photo
 
 
 def photo_list(request):
-    photo_qs = Photo.objects.all()
+    photo_qs = Photo.objects.all().prefetch_related('photo_comment_set').select_related('author')
     return render(request, 'photos/list.html', {
         'photo_list': photo_qs,
     })
 
+
+def photo_detail(request, pk):
+    photo = get_object_or_404(Photo.objects.select_related('author'), id=pk)
+    return render(request, 'photos/detail.html', {
+        'photo': photo,
+    })
