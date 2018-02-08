8c73906628a058ee8612997289267cfe16b15c68
허재영 <jay.jaeyoung@gmail.com>
2018-02-08 13:28:41 +0900

    Create validator for phone_number Model Field

--- tourblog/accounts/models.py
+++ tourblog/accounts/models.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.db import models
+from .validators import phone_number_validator
 
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
     address = models.CharField(max_length=30)
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20,
+            validators=[phone_number_validator])
 
 
-    # phone_number에 대한 validator 추가해야함
 
--- tourblog/accounts/validators.py
+++ tourblog/accounts/validators.py
@@ -1,4 +1,8 @@
+import re
+from django.forms import ValidationError
 
 
-class PhoneNumberValidator:
-    pass
+def phone_number_validator(value):
+    p = re.compile('^01\d{8,9}$')
+    if not re.match(value):
+        raise ValidationError('{} is not proper phone number'.format(value))
