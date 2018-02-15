e1e2c0038a25d7d7db33be0af6a19076ff6ea059
허재영 <jay.jaeyoung@gmail.com>
2018-02-15 13:11:55 +0900

    Add column list view

--- tourblog/columns/urls.py
+++ tourblog/columns/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from . import views
+
 
 app_name = 'columns'
 urlpatterns = [
-    
+    url(r'^list/$', views.column_list, name='column_list'),    
 ]
--- tourblog/columns/views.py
+++ tourblog/columns/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def column_list(request):
+    column_qs = Column.objects.all().prefetch_related('tag_set', 'column_comment_set').select_related('author')
+    return render(request, 'column/list.html', {
+        'column_list': column_qs,
+    }
